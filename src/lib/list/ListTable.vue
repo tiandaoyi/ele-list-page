a80e6249fd37df35cfd6cf98c8711d020bc596ee
tiandaoyi <495060071@qq.com>
2021-03-20 09:26:10 +0800

    feat(table-list): add append-input for edit table

--- src/lib/list/ListTable.vue
+++ src/lib/list/ListTable.vue
@@ -244,7 +244,10 @@
                     @focus="($event) => item.editHover && item.editHover(scope.row[item[prop]], scope.$index, item.prop)"
                     :min="item.editMin"
                     size="small"
-                  ></el-input>
+                  >
+                    <template slot="append" v-if="item.appendKey">{{scope.row[item[appendKey]]}}</template>
+                    <template slot="prepend" v-if="item.prependKey">{{scope.row[item[prependKey]]}}</template>
+                  </el-input>
                 </template>
               </el-form-item>
             </template>
